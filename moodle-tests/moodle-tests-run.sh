5a15626097062c7a60878fb56ede95e14026973f
AdiMac <adamsken1221@gmail.com>
2015-10-08 10:25:27 +0200

    changed pushTestReportsToRemoteRepo specifications

--- moodle-tests/moodle-tests-run.sh
+++ moodle-tests/moodle-tests-run.sh
@@ -43,8 +43,9 @@ runMoodletests(){
 
 pushTestReportsToRemoteRepo(){
 	git -C $BASE_TEST_DIR/moodle-test-reports init
-	git -C $BASE_TEST_DIR/moodle-test-reports remote add origin https://github.com/adini121/test-reports.git
+	git -C $BASE_TEST_DIR/moodle-test-reports remote add origin https://adini121:adsad1221@github.com/adini121/test-reports.git
 	git -C $BASE_TEST_DIR/moodle-test-reports fetch
+	git -C $BASE_TEST_DIR/moodle-test-reports checkout moodle-test-reports
 	git -C $BASE_TEST_DIR/moodle-test-reports pull origin moodle-test-reports
 	git -C $BASE_TEST_DIR/moodle-test-reports add .
 	git -C $BASE_TEST_DIR/moodle-test-reports commit -m "test report test_reports_"$MoodleVersion".log for version $MoodleVersion"
