b0d0cf48eebe278d9ca9022c39ae0dc730ddbfb6
AdiMac <adamsken1221@gmail.com>
2015-10-12 22:45:48 +0200

    modified configureMoodleTests()

--- moodle-tests/moodle-tests-run.sh
+++ moodle-tests/moodle-tests-run.sh
@@ -44,6 +44,7 @@ startMoodle_SeleniumHub(){
 	export DISPLAY=:0.0
 	sleep 3
 	/usr/bin/java -jar '$BASE_TEST_DIR'/test_'$moodleInstance'/lib/selenium-2.47.1/selenium-server-standalone-2.47.1.jar -role hub -hub http://localhost:4444/grid/register 2>&1 | tee '$BASE_TEST_DIR'/moodle-test-reports/Selenium-Hub-output.log
+	sleep 2
 	tmux detach'
 	# sleep 5
 	# echo "exiting tmux session selenium_hub"
@@ -53,8 +54,9 @@ startMoodle_SeleniumNode(){
 	echo "starting tmux session selenium-node"
 	tmux new -d -A -s selenium-node '
 	export DISPLAY=:0.0
-	sleep 2
+	sleep 3
 	/usr/bin/java -jar '$BASE_TEST_DIR'/test_'$moodleInstance'/lib/selenium-2.47.1/selenium-server-standalone-2.47.1.jar -role node -hub http://localhost:4444/grid/register 2>&1 | tee '$BASE_TEST_DIR'/moodle-test-reports/test_log_from_SeNode_'$MoodleVersion'.log
+	sleep 2
 	tmux detach'
 	# sleep 5
 	# echo "exiting tmux session selenium-node"
