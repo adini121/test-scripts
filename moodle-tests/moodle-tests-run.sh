e375d3f5085cb241fd26d7235a30bcbd054dbf36
AdiMac <adamsken1221@gmail.com>
2015-10-12 17:39:19 +0200

    fixed tmux issues

--- moodle-tests/moodle-tests-run.sh
+++ moodle-tests/moodle-tests-run.sh
@@ -41,18 +41,19 @@ startMoodle_SeleniumHub(){
 	export DISPLAY=:0.0
 	sleep 1
 	java -jar $BASE_TEST_DIR/test_$moodleInstance/lib/selenium-2.47.1/selenium-server-standalone-2.47.1.jar -role hub -hub http://localhost:4444/grid/register
-	echo "exiting tmux session selenium_hub" '
-	tmux detach
+	echo "exiting tmux session selenium_hub"
+	tmux detach'
 }
 
 startMoodle_SeleniumNode(){
 	echo "starting tmux session selenium-node"
 	tmux new -A -s selenium-node '
 	export DISPLAY=:0.0
-	sleep 1
+	sleep 2
 	java -jar $BASE_TEST_DIR/test_$moodleInstance/lib/selenium-2.47.1/selenium-server-standalone-2.47.1.jar -role node -hub http://localhost:4444/grid/register 2>&1 | tee $BASE_TEST_DIR/moodle-test-reports/test_log_from_SeNode_"$MoodleVersion".log
-	echo "exiting tmux session selenium-node" '
-	tmux detach
+	sleep 2
+	echo "exiting tmux session selenium-node"
+	tmux detach'
 }
 
 configureMoodleTests(){
