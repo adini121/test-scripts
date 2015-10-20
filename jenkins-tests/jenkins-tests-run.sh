953e6bac0879d0d2f0d31415a63cb6afadc85bff
AdiMac <adamsken1221@gmail.com>
2015-10-20 14:35:30 +0200

    Changelog: fixed remote git URL error

--- jenkins-tests/jenkins-tests-run.sh
+++ jenkins-tests/jenkins-tests-run.sh
@@ -26,7 +26,7 @@ if [ ! -d $JENKINS_Test_DIR ]; then
 fi 
 
 if [ ! -d $JENKINS_Test_DIR/Jenkins_$Test_Instance ]; then
-    git -C $JENKINS_Test_DIR clone --recursive Jenkins_$Test_Instance
+    git -C $JENKINS_Test_DIR clone --recursive https://github.com/jenkinsci/acceptance-test-harness.git Jenkins_$Test_Instance
 fi
 
 git -C $JENKINS_Test_DIR/Jenkins_$Test_Instance pull
