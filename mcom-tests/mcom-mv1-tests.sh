cb1387ab0666c046370621a8e5deab2d52faa1bf
AdiMac <adamsken1221@gmail.com>
2015-12-21 23:40:07 +0100

    mcom : fix test command

--- mcom-tests/mcom-mv1-tests.sh
+++ mcom-tests/mcom-mv1-tests.sh
@@ -71,7 +71,7 @@ sleep 2
 
 runBedrocktests(){
 	#export DISPLAY=:0.0
-	py.test -r=fsxXR --verbose --baseurl=http://134.96.235.47:$BedrockPort --host 134.96.235.159 --port 1235 --browsername=firefox --capability=browser:FIREFOX_30_WINDOWS_8_64 --capability=apikey:c717c5b3-a307-461e-84ea-1232d44cde89 --capability=email:test@testfabrik.com --capability=record:false --capability=extract:false --credentials=credentials.yaml --platform=MAC --destructive 2>&1 | tee $REPORTS_DIR/"$currentTime"_BedrockTests_mv1_"$BedrockGitTag".log
+	py.test -r=fsxXR --verbose --baseurl=http://134.96.235.47:$BedrockPort --host 134.96.235.159 --port 1235 --browsername=firefox --capability=browser:FIREFOX_30_WINDOWS_8_64 --capability=apikey:c717c5b3-a307-461e-84ea-1232d44cde89 --capability=email:test@testfabrik.com --capability=record:false --capability=extract:false --credentials=credentials.yaml --platform=MAC --destructive tests/. 2>&1 | tee $REPORTS_DIR/"$currentTime"_BedrockTests_mv1_"$BedrockGitTag".log
 }
 
 
