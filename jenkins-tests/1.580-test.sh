be8f28439bd81787bfc75e0dd6396e38ca5dafcf
AdiMac <adamsken1221@gmail.com>
2015-12-26 22:48:11 +0100

    Jenkins : save sessionID to DB

--- jenkins-tests/1.580-test.sh
+++ jenkins-tests/1.580-test.sh
@@ -11,6 +11,7 @@ usage(){
         echo "  -v <JenkinsVersion>     Jenkins version - Git Tag (e.g. 1.600, 1.615)"
         echo "  -s <startupPort>        Tomcat startup port (e.g. 8082)"
         echo " 	-i <TestInstance>		Jenkins Test Repository Instance (e.g. first, second, third)	"
+        echo "  -v <JenkinsVersion>     DatabaseSessionIDsVersion (e.g. 1_600, 1_615)"
         exit 1
 }
 
@@ -35,13 +36,20 @@ fi
 }
 
 gatherTestReports(){
-
-	if [ ! -f /home/nisal/Dropbox/TestResults/Jenkins/core_1.580_ath_reports_"$JenkinsVersion".log ];then
-			touch /home/nisal/Dropbox/TestResults/Jenkins/core_1.580_ath_reports_"$JenkinsVersion".log
-	fi
-	#if [ ! -f /home/nisal/Dropbox/TestResults/Jenkins/plugins_1.580_ath_reports_"$JenkinsVersion".log ];then
-	#		touch /home/nisal/Dropbox/TestResults/Jenkins/plugins_1.580_ath_reports_"$JenkinsVersion".log
-#	fi
+currentTime=$(date "+%Y.%m.%d")
+REPORTS_DIR="/dddhome/nisal/Dropbox/TestResults/Jenkins"
+if [ ! -f $REPORTS_DIR/core_1.580_ath_reports_"$JenkinsVersion".log ];then
+    	touch $REPORTS_DIR/core_1.580_ath_reports_"$JenkinsVersion".log
+fi
+if [ ! -f $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log ];then
+		touch $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log
+fi
+mysql -u root << EOF
+use jenkins_core_sessionIDs;
+DROP TABLE IF EXISTS sessionids_$MoodleVersion;
+EOF
+sed -i 's|test_session_ids|sessionids_'$DatabaseSessionIDsVersion'|g' $JENKINS_Test_DIR/Jenkins_1.580_ath_$TestInstance/src/main/java/org/jenkinsci/test/acceptance/utils/SeleniumGridConnection.java
+sed -i 's|.*FileWriter fileWriter.*|            FileWriter fileWriter = new FileWriter("'$REPORTS_DIR'/'$currentTime'_BrowserIdList_'$JenkinsVersion'.log", true);|g' $JENKINS_Test_DIR/Jenkins_1.580_ath_$TestInstance/src/main/java/org/jenkinsci/test/acceptance/utils/SeleniumGridConnection.java
 }
 
 
@@ -49,21 +57,23 @@ runJenkinsTests(){
 echo "..............................................runJenkinsTests.............................................."
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 cd $JENKINS_Test_DIR/Jenkins_1.580_ath_$TestInstance
-export JAVA_OPTS="-Djava.io.tmpdir=/home/$user/jenkinsHome/jenkinsHome$JenkinsVersion/tmp"
-TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/core/*Test test 2>&1 | tee /home/nisal/Dropbox/TestResults/Jenkins/core_1.580_ath_reports_"$JenkinsVersion".log
-#TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/plugins/*Test test 2>&1 | tee /home/nisal/Dropbox/TestResults/Jenkins/plugins_1.580_ath_reports_"$JenkinsVersion".log
+TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/core/*Test test 2>&1 | tee $REPORTS_DIR/core_1.580_ath_reports_"$JenkinsVersion".log
+#TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/plugins/*Test test 2>&1 | tee $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log
 }
 
 cleanup(){
 echo "_________Cleaning all processes and directories left behind by this jenkins instance____________"
+sleep 5
 kill $(ps aux | grep -E 'nisal.*java -jar /tmp*' | awk '{print $2}')
 kill $(ps aux | grep -E 'nisal.*slave*' | awk '{print $2}')
 kill $(ps aux | grep -E '/usr/lib/jvm/java.*TomcatInstance'$startupPort'*' | awk '{print $2}')
 echo "Deleting Jenkins TMP directory"
-rm -rf /home/$user/jenkinsHome/jenkinsHome$JenkinsVersion/tmp
+cd /tmp
+rm -rf $(ls -la | grep nisal | awk '{print $9}')
+echo "done"
 }
 
-while getopts ":u:v:s:i:" i; do
+while getopts ":u:v:s:i:d:" i; do
         case "${i}" in
         u) user=${OPTARG}
         ;;
@@ -72,13 +82,14 @@ while getopts ":u:v:s:i:" i; do
         s) startupPort=${OPTARG}
 		;;
 		i) TestInstance=${OPTARG}
-
+        ;;
+        d) DatabaseSessionIDsVersion
         esac
 done
 
 shift $((OPTIND - 1))
 
-if [[ $user == "" || $JenkinsVersion == "" || $startupPort == "" || $TestInstance == "" ]]; then
+if [[ $user == "" || $JenkinsVersion == "" || $startupPort == "" || $TestInstance == "" || $DatabaseSessionIDsVersion == "" ]]; then
         usage
 fi
 
