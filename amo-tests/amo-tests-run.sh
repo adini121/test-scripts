858b83d935906ab5a24d7087ce7eaa04015e04fe
AdiMac <adamsken1221@gmail.com>
2015-12-27 14:10:57 +0100

    AMO finding proper commits

--- amo-tests/amo-tests-run.sh
+++ amo-tests/amo-tests-run.sh
@@ -7,85 +7,63 @@ usage(){
         echo "Usage: $0 <OPTIONS>"
         echo "Required options:"
         echo "  -u $USER            user name"
-        echo "  -t <AMOGitTag>      AMO git tag eg: 2015.09.08 | 2015.09.15 | 2015.09.22"
+        echo "  -t <AMOGitTag_underscores>      AMO git tag eg: 2015_09_08 | 2015_09_15 | 2015_09_22"
         echo "  -m <AMOInstance>    Eg AMO_first, AMO_second"
-        echo "  -p <AMOPort>        Eg 8088, 8089"
+        echo "  -p <AMOPort>        Eg 9001, 8088, 8089"
+        echo "  -c <CommitHash>		CommitHash"
         exit 1
 }
-
-
 mkdir -p /home/$USER/AMOHome
 AMOBaseDir="/home/$USER/AMOHome"
 
 installTestingCode(){
 echo "................................installing AMO test code......................................."
-			
-	echo "AMO dir will be test_$AMOInstance"
-		if [ ! -d $AMOBaseDir/test_$AMOInstance ]; then
-			git -C $AMOBaseDir clone --recursive https://github.com/mozilla/Addon-Tests test_$AMOInstance
-		fi
- 	
-	git -C $AMOBaseDir/test_$AMOInstance pull
-	git -C $AMOBaseDir/test_$AMOInstance submodule update --init
+echo "AMO dir will be test_$AMOInstance"
+if [ ! -d $AMOBaseDir/test_$AMOInstance ]; then
+		git -C $AMOBaseDir clone https://github.com/mozilla/Addon-Tests test_$AMOInstance
+fi
+git -C $AMOBaseDir/test_$AMOInstance stash
+git -C $AMOBaseDir/test_$AMOInstance pull
+git -C $AMOBaseDir/test_$AMOInstance checkout $CommitHash
 }
 
 gatherTestReports(){
-mkdir -p $AMOBaseDir/AMO-test-reports
-	if [ ! -f $AMOBaseDir/AMO-test-reports/test_reports_"$AMOGitTag".log ] || [ ! -f $AMOBaseDir/AMO-test-reports/test_log_from_SeNode_"$AMOGitTag".log ] || [ ! -f $AMOBaseDir/AMO-test-reports/selenium-hub-AMO-output.log ];
-		then
-			touch $AMOBaseDir/AMO-test-reports/test_reports_"$AMOGitTag".log
-			touch $AMOBaseDir/AMO-test-reports/test_log_from_SeNode_"$AMOGitTag".log
-			touch $AMOBaseDir/AMO-test-reports/selenium-hub-AMO-output.log
-	fi
-}
-
-startAMO_SeleniumHub(){
-	echo "starting tmux session selenium-hub-AMO "
-	tmux kill-session -t selenium-hub-AMO
-	tmux new -d -A -s selenium-hub-AMO '
-	export DISPLAY=:0.0
-	sleep 3
-	/usr/bin/java -jar /home/'$USER'/selenium-server-standalone-2.47.1.jar -role hub -hub http://localhost:4444/grid/register 2>&1 | tee '$AMOBaseDir'/AMO-test-reports/selenium-hub-AMO-output.log
-	sleep 2
-	tmux detach'
-	# sleep 5
-	# echo "exiting tmux session selenium_hub"
-}
-
-startAMO_SeleniumNode(){
-	echo "starting tmux session selenium-node-AMO-AMO"
-	tmux kill-session -t selenium-node-AMO
-	tmux new -d -A -s selenium-node-AMO '
-	export DISPLAY=:0.0
-	sleep 3
-	/usr/bin/java -jar /home/'$USER'/selenium-server-standalone-2.47.1.jar -role node -hub http://localhost:4444/grid/register -browser browserName=firefox -platform platform=LINUX 2>&1 | tee '$AMOBaseDir'/AMO-test-reports/test_log_from_SeNode_'$AMOGitTag'.log
-	sleep 2
-	tmux detach'
-	# sleep 5
-	# echo "exiting tmux session selenium-node-AMO"
+currentTime=$(date "+%Y.%m.%d-%H.%M")
+echo "Current Time : $currentTime"
+REPORTS_DIR="/home/$USER/Dropbox/TestResults/AMO"
+echo "Reports directory is: "$REPORTS_DIR" "
+if [ ! -f $REPORTS_DIR/"$currentTime"_AMOTests_"$CommitHash"_"$AMOGitTag_underscores".log ];then
+	touch $REPORTS_DIR/"$currentTime"_AMOTests_"$CommitHash"_"$AMOGitTag_underscores".log
+fi
 }
 
 configureAMOTests(){
 echo "................................configuring AMO test-properties......................................."
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "CURRENT_DIR="$CURRENT_DIR""
+mysql -u root << EOF
+use amo_sessionIDs;
+DROP TABLE IF EXISTS sessionids_$AMOGitTag_underscores;
+EOF
+# sed -i 's|test_session_ids|sessionids_'$AMOGitTag_underscores'|g' $AMOBaseDir/test_$AMOInstance/conftest.py
+# sed -i 's|/home/adi/python.txt|'$REPORTS_DIR'/'$currentTime'_BrowserIdList_'$AMOGitTag_underscores'.log|g' $AMOBaseDir/test_$AMOInstance/conftest.py
 cp $CURRENT_DIR/amo_variables.json $AMOBaseDir/test_$AMOInstance
-
 }
 
 configureVirtualenv(){
-	echo "................................configuring AMO Virtualenv......................................."
-	curl -sL https://raw.github.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh | $SHELL
-	source /home/$USER/.venvburrito/startup.sh
-	cd $AMOBaseDir/test_$AMOInstance
-	mkvirtualenv test_$AMOInstance
-	pip install -r requirements.txt
-	sleep 2
+echo "................................configuring AMO Virtualenv......................................."
+cd $AMOBaseDir/test_$AMOInstance
+pip install virtualenv
+virtualenv $AMOInstance
+source $AMOInstance/bin/activate
+pip install -r requirements.txt
+pip install mysql-connector-python --allow-external mysql-connector-python
+sleep 1
 }
 
 runAMOtests(){
-	#export DISPLAY=:0.0
-	py.test --baseurl=http://localhost:$AMOPort --browsername=firefox --variables=amo_variables.json --platform=linux --destructive tests/desktop/ 2>&1 | tee $AMOBaseDir/AMO-test-reports/test_reports_"$AMOGitTag".log
+#export DISPLAY=:0.0
+py.test  -r=fsxXR --verbose --baseurl=http://134.96.235.47:$AMOPort --host 134.96.235.159 --port 1235 --browsername=firefox --capability=browser:FIREFOX_30_WINDOWS_8_64 --capability=email:test@testfabrik.com --capability=record:false --capability=extract:false --capability=apikey:c717c5b3-a307-461e-84ea-1232d44cde89 --variables=amo_variables.json --platform=MAC --destructive tests/desktop/ 2>&1 | tee $REPORTS_DIR/"$currentTime"_AMOTests_"$CommitHash"_"$AMOGitTag_underscores".log
 }
 
 
@@ -94,17 +72,19 @@ while getopts ":u:t:m:p:" i; do
     case "${i}" in
         u) USER=${OPTARG}
         ;;
-        t) AMOGitTag=${OPTARG}
+        t) AMOGitTag_underscores=${OPTARG}
         ;;
         m) AMOInstance=${OPTARG}
         ;;
         p) AMOPort=${OPTARG}
+		;;
+		c) CommitHash=${OPTARG}
     esac
 done
 
 shift $((OPTIND - 1))
 
-if [[ $USER == "" || $AMOGitTag == "" || $AMOInstance == "" || $AMOPort == "" ]]; then
+if [[ $USER == "" || $AMOGitTag_underscores == "" || $AMOInstance == "" || $AMOPort == "" || $CommitHash == "" ]]; then
         usage
 fi
 
@@ -113,10 +93,6 @@ fi
 installTestingCode
 
 gatherTestReports
-
-startAMO_SeleniumHub
-
-startAMO_SeleniumNode
 
 configureAMOTests
 
