ea430dfa0f72a5917525f6e26964e081e7f6c28b
AdiMac <adamsken1221@gmail.com>
2015-12-29 17:30:38 +0100

    AMO mv1 and MV2 modifications/additions

--- amo-tests/MV2-amo-tests-run.sh
+++ amo-tests/MV2-amo-tests-run.sh
@@ -17,13 +17,13 @@ AMOBaseDir="/home/$USER/AMOHome"
 
 installTestingCode(){
 echo "................................installing AMO test code......................................."
-echo "AMO dir will be test_mv1_$AMOInstance"
-if [ -d $AMOBaseDir/test_mv1_$AMOInstance ]; then
-	rm -rf $AMOBaseDir/test_mv1_$AMOInstance
+echo "AMO dir will be test_MV2_$AMOInstance"
+if [ -d $AMOBaseDir/test_MV2_$AMOInstance ]; then
+	rm -rf $AMOBaseDir/test_MV2_$AMOInstance
 fi
-git -C $AMOBaseDir clone -b addons-mv1 --single-branch git@github.com:adini121/Addon-Tests.git test_mv1_$AMOInstance
-git -C $AMOBaseDir/test_mv1_$AMOInstance stash
-git -C $AMOBaseDir/test_mv1_$AMOInstance fetch
+git -C $AMOBaseDir clone -b addons-MV2 --single-branch git@github.com:adini121/Addon-Tests.git test_MV2_$AMOInstance
+git -C $AMOBaseDir/test_MV2_$AMOInstance stash
+git -C $AMOBaseDir/test_MV2_$AMOInstance fetch
 }
 
 gatherTestReports(){
@@ -31,12 +31,12 @@ currentTime=$(date "+%Y.%m.%d-%H.%M")
 echo "Current Time : $currentTime"
 REPORTS_DIR="/home/$USER/Dropbox/TestResults/AMO"
 echo "Reports directory is: "$REPORTS_DIR" "
-if [ ! -f $REPORTS_DIR/"$currentTime"_AMOTests_MV1_"$AMOGitTag".log ];then
-	touch $REPORTS_DIR/"$currentTime"_AMOTests_MV1_"$AMOGitTag".log
+if [ ! -f $REPORTS_DIR/"$currentTime"_AMOTests_MV2_"$AMOGitTag".log ];then
+	touch $REPORTS_DIR/"$currentTime"_AMOTests_MV2_"$AMOGitTag".log
 fi
 
-if [ ! -f $REPORTS_DIR/"$currentTime"_AMO_BrowserIdList_mv1_"$AMOGitTag".log ];then
-    touch $REPORTS_DIR/"$currentTime"_AMO_BrowserIdList_mv1_"$AMOGitTag".log
+if [ ! -f $REPORTS_DIR/"$currentTime"_AMO_BrowserIdList_MV2_"$AMOGitTag".log ];then
+    touch $REPORTS_DIR/"$currentTime"_AMO_BrowserIdList_MV2_"$AMOGitTag".log
 fi
 }
 
@@ -48,14 +48,14 @@ mysql -u root << EOF
 use amo_sessionIDs;
 DROP TABLE IF EXISTS sessionids_$AMOGitTag;
 EOF
-sed -i 's|test_session_ids|sessionids_'$AMOGitTag'|g' $AMOBaseDir/test_mv1_$AMOInstance/conftest.py
-sed -i 's|/home/nisal/python.txt|'$REPORTS_DIR'/'$currentTime'_AMO_BrowserIdList_'$AMOGitTag'.log|g' $AMOBaseDir/test_mv1_$AMOInstance/conftest.py
-cp $CURRENT_DIR/credentials.yaml $AMOBaseDir/test_mv1_$AMOInstance/credentials.yaml
+sed -i 's|test_session_ids|sessionids_'$AMOGitTag'|g' $AMOBaseDir/test_MV2_$AMOInstance/conftest.py
+sed -i 's|/home/nisal/python.txt|'$REPORTS_DIR'/'$currentTime'_AMO_BrowserIdList_'$AMOGitTag'.log|g' $AMOBaseDir/test_MV2_$AMOInstance/conftest.py
+cp $CURRENT_DIR/credentials.yaml $AMOBaseDir/test_MV2_$AMOInstance/credentials.yaml
 }
 
 configureVirtualenv(){
 echo "................................configuring AMO Virtualenv......................................."
-cd $AMOBaseDir/test_mv1_$AMOInstance
+cd $AMOBaseDir/test_MV2_$AMOInstance
 pip install virtualenv
 virtualenv $AMOInstance
 source $AMOInstance/bin/activate
@@ -66,7 +66,7 @@ sleep 2
 
 runAMOtests(){
 #export DISPLAY=:0.0
-py.test  -r=fsxXR --verbose --baseurl=http://134.96.235.47:$AMOPort --host 134.96.235.159 --port 1235 --browsername=firefox --capability=browser:FIREFOX_30_WINDOWS_8_64 --capability=email:test@testfabrik.com --capability=record:true --capability=extract:true --capability=apikey:c717c5b3-a307-461e-84ea-1232d44cde89 --credentials=credentials.yaml --platform=MAC --destructive tests/desktop/ 2>&1 | tee $REPORTS_DIR/"$currentTime"_AMOTests_MV1_"$AMOGitTag".log
+py.test  -r=fsxXR --verbose --baseurl=http://134.96.235.47:$AMOPort --host 134.96.235.159 --port 1235 --browsername=firefox --capability=browser:FIREFOX_30_WINDOWS_8_64 --capability=email:test@testfabrik.com --capability=record:true --capability=extract:true --capability=apikey:c717c5b3-a307-461e-84ea-1232d44cde89 --credentials=credentials.yaml --platform=MAC --destructive tests/desktop/ 2>&1 | tee $REPORTS_DIR/"$currentTime"_AMOTests_MV2_"$AMOGitTag".log
 }
 
 
