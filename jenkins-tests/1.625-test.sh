903471ecbb6d8227b9139ac6ee2df9ae3e9b5eed
AdiMac <adamsken1221@gmail.com>
2016-01-05 00:44:52 +0100

    Jenkins ready 1.625-test.sh

--- jenkins-tests/1.625-test.sh
+++ jenkins-tests/1.625-test.sh
@@ -5,16 +5,14 @@
 
 
 usage(){
-        echo "Usage: $0 <OPTIONS>"
-        echo ".........................................."
-        echo " WITHOUT EXTRACT AND RECORD"
-        echo ".........................................."
-        echo "Required options:"
-        echo "  -u <UID>                user name (e.g. adi)"
-        echo "  -v <JenkinsVersion>     Jenkins version - Git Tag (e.g. 1.600, 1.615)"
-        echo "  -s <startupPort>        Tomcat startup port (e.g. 8082)"
-        echo " 	-i <TestInstance>		Jenkins Test Repository Instance (e.g. first, second, third)	"
-        exit 1
+echo "Usage: $0 <OPTIONS>"
+echo "Required options:"
+echo "  -u <UID>                user name (e.g. adi)"
+echo "  -v <JenkinsVersion>     Jenkins version - Git Tag (e.g. 1.600, 1.615)"
+echo "  -s <startupPort>        Tomcat startup port (e.g. 8082)"
+echo " 	-i <TestInstance>		Jenkins Test Repository Instance (e.g. first, second, third)	"
+echo "  -d <JenkinsVersion>     Database SessionIDs Version (e.g. 1_600, 1_615)"
+exit 1
 }
 
 downloadJenkinsTestSuite(){
@@ -29,26 +27,38 @@ if [ ! -d $JENKINS_Test_DIR ]; then
 fi 
 
 if [ ! -d $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance ]; then
-    git -C $JENKINS_Test_DIR clone -b 1.625-117 --single-branch git@github.com:adini121/acceptance-test-harness.git Jenkins_1.625_ath_$TestInstance
+    git -C $JENKINS_Test_DIR clone -b 1.625-ath --single-branch git@github.com:adini121/acceptance-test-harness.git Jenkins_1.625_ath_$TestInstance
 else
     rm -rf $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance
-    git -C $JENKINS_Test_DIR clone -b 1.625-117 --single-branch git@github.com:adini121/acceptance-test-harness.git Jenkins_1.625_ath_$TestInstance
+    git -C $JENKINS_Test_DIR clone -b 1.625-ath --single-branch git@github.com:adini121/acceptance-test-harness.git Jenkins_1.625_ath_$TestInstance
 fi
 
 }
 
 gatherTestReports(){
+currentTime=$(date "+%Y.%m.%d-%H.%M")
+REPORTS_DIR="/home/nisal/Dropbox/TestResults/Jenkins"
+if [ ! -f $REPORTS_DIR/core_1.625_ath_reports_"$JenkinsVersion".log ];then
+    	touch $REPORTS_DIR/core_1.625_ath_reports_"$JenkinsVersion".log
+fi
+if [ ! -f $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log ];then
+		touch $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log
+fi
+mysql -u root << EOF
+use jenkins_core_sessionIDs;
+DROP TABLE IF EXISTS sessionids_$DatabaseSessionIDsVersion;
+EOF
 
-	if [ ! -f /home/nisal/Dropbox/TestResults/Jenkins/1.625_ath_reports_"$JenkinsVersion".log ];then
-			touch /home/nisal/Dropbox/TestResults/Jenkins/1.625_ath_reports_"$JenkinsVersion".log
-	fi
+sed -i 's|test_session_ids|sessionids_'$DatabaseSessionIDsVersion'|g' $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance/src/main/java/org/jenkinsci/test/acceptance/utils/SeleniumGridConnection.java
+sed -i 's|.*FileWriter fileWriter.*|            FileWriter fileWriter = new FileWriter("'$REPORTS_DIR'/'$currentTime'_BrowserIdList_'$JenkinsVersion'.log", true);|g' $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance/src/main/java/org/jenkinsci/test/acceptance/utils/SeleniumGridConnection.java
 }
 
 
 runJenkinsTests(){
 echo "..............................................runJenkinsTests.............................................."
 cd $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance
-TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/core/*Test test 2>&1 | tee /home/nisal/Dropbox/TestResults/Jenkins/1.625_ath_reports_"$JenkinsVersion".log
+TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/core/*Test test 2>&1 | tee $REPORTS_DIR/core_1.625_ath_reports_"$JenkinsVersion".log
+#TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn -Dtest=**/plugins/*Test test 2>&1 | tee $REPORTS_DIR/"$currentTime"_BrowserIdList_"$JenkinsVersion".log
 }
 
 
@@ -61,13 +71,14 @@ while getopts ":u:v:s:i:" i; do
         s) startupPort=${OPTARG}
 		;;
 		i) TestInstance=${OPTARG}
-
+        ;;
+        d) DatabaseSessionIDsVersion=${OPTARG}
         esac
 done
 
 shift $((OPTIND - 1))
 
-if [[ $user == "" || $JenkinsVersion == "" || $startupPort == "" || $TestInstance == "" ]]; then
+if [[ $user == "" || $JenkinsVersion == "" || $startupPort == "" || $TestInstance == "" || $DatabaseSessionIDsVersion == "" ]]; then
         usage
 fi
 
