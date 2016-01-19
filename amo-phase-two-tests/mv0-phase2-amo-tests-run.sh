0d935969ea9db07bef9184d3d5469120f1b32d20
AdiMac <adamsken1221@gmail.com>
2016-01-19 13:44:09 +0100

    AMO p2 dbsetings conftest

--- amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
+++ amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
@@ -57,33 +57,41 @@ cat > $AMOBaseDir/phase2_test_mv0_$AMOInstance/dbconftest.py << EOF
 """ Print sessionID to database and file """
 import MySQLdb
 from time import gmtime, strftime
-current_time = strftime("%Y-%m-%d %H:%M")
-print('Current time is: {}'.format(current_time))
+@pytest.fixture(autouse=True)
+def session_id(mozwebqa):
+    print 'Session ID: {}'.format(mozwebqa.selenium.session_id)
+    str = '{}\n'.format(mozwebqa.selenium.session_id)
+    str_session_id = '{}'.format(mozwebqa.selenium.session_id)
 
-""" Connect to MySQL database """
-try:
-    conn = MySQLdb.connect(host='localhost',
-                           user='root',
-                           passwd='',
-                           db='phase_two_amo_sids')
+    with open ("/home/nisal/python.txt", "a") as myfile:
+        myfile.write(str)
 
-    c = conn.cursor()
-    tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
-        session_id VARCHAR(60) not NULL,
-        date_created VARCHAR(100) not NULL,
-        primary key(id))"""
-    c.execute(tblQuery)
-    print('............Successfully created table .......')
-    insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
-    c = conn.cursor()
-    c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
-    print('............Successfully ADDED to table .......')
-    conn.commit()
-except:
-    print ('UNABLE TO PERFORM DATABASE OPERATION')
+    current_time = strftime("%Y-%m-%d %H:%M")
+    print('Current time is: {}'.format(current_time))
+    """ Connect to MySQL database """
+    try:
+        conn = MySQLdb.connect(host='localhost',
+                               user='root',
+                               passwd='',
+                               db='phase_two_amo_sids')
 
-finally:
-    conn.close()
+        c = conn.cursor()
+        tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
+            session_id VARCHAR(60) not NULL,
+            date_created VARCHAR(100) not NULL,
+            primary key(id))"""
+        c.execute(tblQuery)
+        print('............Successfully created table .......')
+        insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
+        c = conn.cursor()
+        c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
+        print('............Successfully ADDED to table .......')
+        conn.commit()
+    except:
+        print ('UNABLE TO PERFORM DATABASE OPERATION')
+
+    finally:
+        conn.close()
 EOF
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sleep 2
