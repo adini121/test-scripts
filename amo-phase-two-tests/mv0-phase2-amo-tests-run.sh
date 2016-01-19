3434c827ba5dae0f9fbf7bc0f960defb13775d6c
AdiMac <adamsken1221@gmail.com>
2016-01-19 13:37:00 +0100

    AMO p2 dbsetings conftest

--- amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
+++ amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
@@ -58,7 +58,10 @@ cat > $AMOBaseDir/phase2_test_mv0_$AMOInstance/dbconftest.py << EOF
 import MySQLdb
 from time import gmtime, strftime
 current_time = strftime("%Y-%m-%d %H:%M")
-    print('Current time is: {}'.format(current_time))
+print('Current time is: {}'.format(current_time))
+
+""" Connect to MySQL database """
+print('Current time is: {}'.format(current_time))
     """ Connect to MySQL database """
     try:
         conn = MySQLdb.connect(host='localhost',
@@ -74,10 +77,8 @@ current_time = strftime("%Y-%m-%d %H:%M")
         c.execute(tblQuery)
         print('............Successfully created table .......')
         insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
-        # insQuery = """insert into test_session_ids (session_id, date_created) values ('whatever', 'whatever')"""
         c = conn.cursor()
         c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
-        # c.execute(insQuery)
         print('............Successfully ADDED to table .......')
         conn.commit()
     except:
