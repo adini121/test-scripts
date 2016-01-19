1ada4507291758475f32a5e33ba21d5d3466a6ef
AdiMac <adamsken1221@gmail.com>
2016-01-19 13:38:46 +0100

    AMO p2 dbsetings conftest

--- amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
+++ amo-phase-two-tests/mv0-phase2-amo-tests-run.sh
@@ -61,31 +61,29 @@ current_time = strftime("%Y-%m-%d %H:%M")
 print('Current time is: {}'.format(current_time))
 
 """ Connect to MySQL database """
-print('Current time is: {}'.format(current_time))
-    """ Connect to MySQL database """
-    try:
-        conn = MySQLdb.connect(host='localhost',
-                               user='root',
-                               passwd='',
-                               db='phase_two_amo_sids')
+try:
+    conn = MySQLdb.connect(host='localhost',
+                           user='root',
+                           passwd='',
+                           db='phase_two_amo_sids')
 
-        c = conn.cursor()
-        tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
-            session_id VARCHAR(60) not NULL,
-            date_created VARCHAR(100) not NULL,
-            primary key(id))"""
-        c.execute(tblQuery)
-        print('............Successfully created table .......')
-        insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
-        c = conn.cursor()
-        c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
-        print('............Successfully ADDED to table .......')
-        conn.commit()
-    except:
-        print ('UNABLE TO PERFORM DATABASE OPERATION')
+    c = conn.cursor()
+    tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
+        session_id VARCHAR(60) not NULL,
+        date_created VARCHAR(100) not NULL,
+        primary key(id))"""
+    c.execute(tblQuery)
+    print('............Successfully created table .......')
+    insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
+    c = conn.cursor()
+    c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
+    print('............Successfully ADDED to table .......')
+    conn.commit()
+except:
+    print ('UNABLE TO PERFORM DATABASE OPERATION')
 
-    finally:
-        conn.close()
+finally:
+    conn.close()
 EOF
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 sleep 2
