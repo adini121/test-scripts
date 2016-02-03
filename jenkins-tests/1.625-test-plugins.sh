1a372ec01fc02a54530392fb3b83d6f9590b1e12
Adi-Infinity <adamsken1221@gmail.com>
2016-02-03 08:07:44 +0100

    1.625-test-plugins.sh few more tests

--- jenkins-tests/1.625-test-plugins.sh
+++ jenkins-tests/1.625-test-plugins.sh
@@ -68,7 +68,7 @@ cd $JENKINS_Test_DIR/Jenkins_1.625_ath_$TestInstance
 TYPE=existing BROWSER=seleniumGrid JENKINS_URL=http://134.96.235.47:$startupPort/jenkins$JenkinsVersion/ mvn \
 -Dmaven.test.skip=false -Dtest=BuildTimeoutPluginTest,JobParameterSummaryPluginTest,HtmlPublisherPluginTest,MailWatcherPluginTest,\
 CoberturaPluginTest,PlotPluginTest,MultipleScmsPluginTest,JavadocPluginTest,DescriptionSetterPluginTest,\
-DashboardViewPluginTest,ProjectDescriptionSetterPluginTest,BatchTaskPluginTest,WsCleanupPluginTest,\
+NestedViewPluginTest,CompressArtifactsPluginTest,MultipleScmsPluginTest,DashboardViewPluginTest,ProjectDescriptionSetterPluginTest,BatchTaskPluginTest,WsCleanupPluginTest,\
 EnvInjectPluginTest,PostBuildScriptPluginTest,MatrixReloadedPluginTest,SubversionPluginNoDockerTest,\
 MailerPluginTest,ViolationsPluginTest,UpstreamDownstreamColumnPluginTest,DescriptionSetterPluginTest,\
 OwnershipPluginTest test 2>&1 | tee $REPORTS_DIR/plugins_1.625_ath_reports_"$CommitHash"_"$JenkinsVersion".log
